8325497e531cc0cd7efeab3d476add351866eb92
martin.castello <castellomn94@gmail.com>
2018-06-09 14:10:40 -0300

    Neo4j prueba

--- src/main/java/BD/ConectarBD.java
+++ src/main/java/BD/ConectarBD.java
@@ -1,4 +1,31 @@
 package BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectarBD {
+
+    Connection connection=null;
+
+    public Connection ConectarBD() {
+        try {
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName); // here is the ClassNotFoundException
+
+            String serverName = "127.0.0.1";
+            String mydatabase = "instricpcion";
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+
+            String username = "root";
+            String password = "";
+            connection = DriverManager.getConnection(url, username, password);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
+    }
 }
